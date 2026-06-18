d1d0b256d6e5d535eb17482f217ca6f4b99cbb52
Ziad003 <ziad.hossain15@gmail.com>
2026-06-19 03:18:35 +0600

    applying global pending

--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -1,17 +1,18 @@
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Sidebar from "../sidebar/Sidebar";
 import './root.css'
 
 const Root = () => {
-
+   const navigation = useNavigation();
+   const isNavigating=Boolean(navigation.location);
   return (
     <div>
       <Header></Header>
       <div className="body">
         <Sidebar></Sidebar>
-
+        {isNavigating && <span></span>}
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
