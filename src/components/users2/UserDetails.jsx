37c838c1a71a67fbe83611799f616d8f134b4dcc
Ziad003 <ziad.hossain15@gmail.com>
2026-06-19 03:26:19 +0600

    see which user from userDetails using useParams

--- src/components/users2/UserDetails.jsx
+++ src/components/users2/UserDetails.jsx
@@ -1,19 +1,22 @@
-import { useLoaderData, useNavigate } from "react-router";
+import { useLoaderData, useNavigate, useParams } from "react-router";
 
 const UserDetails = () => {
-    const userDetails=useLoaderData();
-    console.log(userDetails)
-    const navigate=useNavigate();
+  const userDetails = useLoaderData();
+  console.log(userDetails);
+  const navigate = useNavigate();
 
-    return (
-        <div style={{border:'1px solid green', padding:'10px' }}>
-           <h3>Name: {userDetails.name}</h3>
-           <p>Phone: {userDetails.phone}</p>
-           <p>Email: {userDetails.email}</p>
-           <p>Website: {userDetails.website}</p>
-           <button onClick={()=>navigate(-1)}>Go back</button>
-        </div>
-    );
+  const whichUser = useParams();
+  console.log(whichUser.userId);
+
+  return (
+    <div style={{ border: "1px solid green", padding: "10px" }}>
+      <h3>Name: {userDetails.name}</h3>
+      <p>Phone: {userDetails.phone}</p>
+      <p>Email: {userDetails.email}</p>
+      <p>Website: {userDetails.website}</p>
+      <button onClick={() => navigate(-1)}>Go back</button>
+    </div>
+  );
 };
 
 export default UserDetails;
