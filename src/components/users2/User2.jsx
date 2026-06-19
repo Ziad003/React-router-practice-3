cbabe81f59f31c51c3ebb6c567c94572cb5c4a42
Ziad003 <ziad.hossain15@gmail.com>
2026-06-19 23:38:48 +0600

    Conditionally navigate using <Navigate>

--- src/components/users2/User2.jsx
+++ src/components/users2/User2.jsx
@@ -1,9 +1,10 @@
 import { Suspense, useState } from "react";
-import { NavLink, useNavigate } from "react-router";
+import { Navigate, NavLink, useNavigate } from "react-router";
 import User2Details from "./User2Details";
 
 const User2 = ({ user }) => {
   const [show, setShow] = useState(false);
+  const [visitHome, setVisitHome] = useState(false);
   const { name, email } = user;
   const navigate = useNavigate();
   const manageNavigate = () => {
@@ -12,6 +13,10 @@ const User2 = ({ user }) => {
   const userRes = fetch(
     `https://jsonplaceholder.typicode.com/users/${user.id}`,
   ).then((res) => res.json());
+
+  if (visitHome) {
+    return <Navigate to={"/"}></Navigate>;
+  }
   return (
     <div
       style={{
@@ -31,6 +36,7 @@ const User2 = ({ user }) => {
       <button onClick={() => setShow(!show)}>
         {show ? "Hide" : "Show"} details here
       </button>
+      <button onClick={() => setVisitHome(true)}>Home</button>
 
       {show && (
         <Suspense fallback={<p>Loading...</p>}>
